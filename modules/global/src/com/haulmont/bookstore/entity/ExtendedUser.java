c1ee28554fe7138e5471b33989a6ebcb4fb1de7d
Wor1ock <vanya.yankin9@gmail.com>
2024-09-05 23:12:57 +0400

    fix: удалить transient в extendedUser

--- modules/global/src/com/haulmont/bookstore/entity/ExtendedUser.java
+++ modules/global/src/com/haulmont/bookstore/entity/ExtendedUser.java
@@ -1,18 +1,18 @@
 package com.haulmont.bookstore.entity;
 
-import com.haulmont.chile.core.annotations.MetaProperty;
 import com.haulmont.cuba.core.entity.annotation.Extends;
 import com.haulmont.cuba.security.entity.User;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity(name = "bookstore_ExtendedUser")
 @Extends(User.class)
 public class ExtendedUser extends User {
-    @Transient
-    @MetaProperty
+    @JoinColumn(name = "CUSTOMER_ID")
+    @OneToOne(fetch = FetchType.LAZY)
     private Customer customer;
 
     public Customer getCustomer() {
