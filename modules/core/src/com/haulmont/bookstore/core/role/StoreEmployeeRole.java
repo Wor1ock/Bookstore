bd087b05a0a0441f0e42dd392d8149118569fa11
bibikkkka <alexfil1402@gmail.com>
2024-09-08 22:51:47 +0400

    Проставление разрешения к кнопке "Подтвердить" на экране OnlineOrderEdit

--- modules/core/src/com/haulmont/bookstore/core/role/StoreEmployeeRole.java
+++ modules/core/src/com/haulmont/bookstore/core/role/StoreEmployeeRole.java
@@ -1,25 +1,25 @@
 package com.haulmont.bookstore.core.role;
 
-import com.haulmont.bookstore.entity.Author;
-import com.haulmont.bookstore.entity.Book;
+import com.haulmont.bookstore.entity.*;
 import com.haulmont.cuba.security.app.role.AnnotatedRoleDefinition;
 import com.haulmont.cuba.security.app.role.annotation.*;
 import com.haulmont.cuba.security.entity.EntityOp;
-import com.haulmont.cuba.security.role.EntityAttributePermissionsContainer;
-import com.haulmont.cuba.security.role.EntityPermissionsContainer;
-import com.haulmont.cuba.security.role.ScreenPermissionsContainer;
-import com.haulmont.cuba.security.role.SpecificPermissionsContainer;
+import com.haulmont.cuba.security.role.*;
 
 @Role(name = StoreEmployeeRole.NAME, description = "Сотрудник магазина")
 public class StoreEmployeeRole extends AnnotatedRoleDefinition {
     public final static String NAME = "StoreEmployeeRole";
 
-    @ScreenAccess(screenIds = {"application-bookstore", "bookstore_Book.browse", "bookstore_Author.browse"})
+    @ScreenAccess(screenIds = {"application-bookstore", "bookstore_Book.browse", "bookstore_Author.browse", "bookstore_OnlineOrder.browse", "bookstore_OnlineOrder.edit", "bookstore_Customer.browse"})
     @Override
     public ScreenPermissionsContainer screenPermissions() {
         return super.screenPermissions();
     }
 
+    @EntityAccess(entityClass = OnlineOrder.class, operations = {EntityOp.CREATE, EntityOp.UPDATE, EntityOp.READ, EntityOp.DELETE})
+    @EntityAccess(entityClass = OrderLine.class, operations = EntityOp.READ)
+    @EntityAccess(entityClass = Customer.class, operations = EntityOp.READ)
+    @EntityAccess(entityClass = Address.class, operations = {EntityOp.CREATE, EntityOp.UPDATE, EntityOp.READ, EntityOp.DELETE})
     @EntityAccess(entityClass = Book.class, operations = EntityOp.READ)
     @EntityAccess(entityClass = Author.class, operations = EntityOp.READ)
     @Override
@@ -27,6 +27,10 @@ public class StoreEmployeeRole extends AnnotatedRoleDefinition {
         return super.entityPermissions();
     }
 
+    @EntityAttributeAccess(entityClass = OrderLine.class, view = "*")
+    @EntityAttributeAccess(entityClass = OnlineOrder.class, modify = "*")
+    @EntityAttributeAccess(entityClass = Customer.class, modify = "*")
+    @EntityAttributeAccess(entityClass = Address.class, modify = "*")
     @EntityAttributeAccess(entityClass = Book.class, view = "*")
     @EntityAttributeAccess(entityClass = Author.class, view = "*")
     @Override
@@ -38,5 +42,11 @@ public class StoreEmployeeRole extends AnnotatedRoleDefinition {
     @Override
     public SpecificPermissionsContainer specificPermissions() {
         return super.specificPermissions();
+    }
+
+    @ScreenComponentAccess(screenId = "bookstore_OnlineOrder.edit", modify = "confirmBtn")
+    @Override
+    public ScreenComponentPermissionsContainer screenComponentPermissions() {
+        return super.screenComponentPermissions();
     }
 }
