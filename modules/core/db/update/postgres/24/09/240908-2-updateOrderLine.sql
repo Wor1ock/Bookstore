cad900ebd7940d35a7638bd7814bf91862df63b3
Wor1ock <vanya.yankin9@gmail.com>
2024-09-09 13:00:04 +0400

    fix: удалить лишние скрипты

--- modules/core/db/update/postgres/24/09/240908-2-updateOrderLine.sql
+++
@@ -1,6 +0,0 @@
-update BOOKSTORE_ORDER_LINE set QUANTITY = 0 where QUANTITY is null ;
-alter table BOOKSTORE_ORDER_LINE alter column QUANTITY set not null ;
-update BOOKSTORE_ORDER_LINE set BOOK_TITLE = '' where BOOK_TITLE is null ;
-alter table BOOKSTORE_ORDER_LINE alter column BOOK_TITLE set not null ;
--- update BOOKSTORE_ORDER_LINE set ONLINE_ORDER_ID = <default_value> where ONLINE_ORDER_ID is null ;
-alter table BOOKSTORE_ORDER_LINE alter column ONLINE_ORDER_ID set not null ;
