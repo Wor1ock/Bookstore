c0f30dae8ae94e08f633f50412dbc1cc40e2ddbc
Wor1ock <vanya.yankin9@gmail.com>
2024-09-09 13:02:47 +0400

    fix: добавить дефолтное значение интервала проверки

--- modules/core/src/com/haulmont/bookstore/config/MyAppConfig.java
+++ modules/core/src/com/haulmont/bookstore/config/MyAppConfig.java
@@ -4,9 +4,11 @@ import com.haulmont.cuba.core.config.Config;
 import com.haulmont.cuba.core.config.Property;
 import com.haulmont.cuba.core.config.Source;
 import com.haulmont.cuba.core.config.SourceType;
+import com.haulmont.cuba.core.config.defaults.Default;
 
 @Source(type = SourceType.DATABASE)
 public interface MyAppConfig extends Config {
     @Property("app.notification.delay")
+    @Default(value = "30")
     Integer getNotificationDelay();
 }
