112355113fa15ac350376c79107a1cd6d58482a3
Wor1ock <vanya.yankin9@gmail.com>
2024-09-09 13:11:48 +0400

    fix: уточнить описание в javadoc

--- modules/global/src/com/haulmont/bookstore/service/UserService.java
+++ modules/global/src/com/haulmont/bookstore/service/UserService.java
@@ -1,5 +1,6 @@
 package com.haulmont.bookstore.service;
 
+import com.haulmont.bookstore.entity.Status;
 import com.haulmont.cuba.security.entity.User;
 
 import java.util.List;
@@ -9,7 +10,7 @@ public interface UserService {
     String NAME = "bookstore_UserService";
 
     /**
-     * Находит всех сотрудников магазина, имеющих определённую роль.
+     * Находит всех сотрудников магазина, имеющих роль StoreEmployeeRole.
      * Сотрудники магазина — это пользователи, у которых назначена роль-маркер для работы с заказами.
      *
      * @return список сотрудников магазина (пользователей системы).
